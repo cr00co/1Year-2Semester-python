ccb46a6cec01d0bd3afa1972efeeaf13831e14df
Anton <antonpuh1162@gmail.com>
2026-03-09 01:19:29 +0200

    add our_cache task solution

--- 2026_02_20(decorators)/3_our_cache/main.py
+++ 2026_02_20(decorators)/3_our_cache/main.py
@@ -1,5 +1,29 @@
 def cache(func):
-    pass
+    storage = {}
+    
+    def wrapper(*args):
+        if args not in storage:
+            storage[args] = func(*args)
+            print("added:", args, storage[args])
+        return storage[args]
+    
+    return wrapper
 
+@cache
 def my_sum(a, b):
     return a + b
+
+@cache
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+if __name__ == "__main__":
+    print(my_sum(1, 2))
+    print(my_sum(1, 2))
+    print(my_sum(42, 42))
+    print(my_sum(42, 42))
+
+    print(fib(10))
+
