5c2998ffbe34cda71b13f5063a85ab4395e04313
Anton <antonpuh1162@gmail.com>
2026-03-23 19:18:50 +0200

    add hw retry

--- homework/2026_02_27/retry/my_decorator.py
+++ homework/2026_02_27/retry/my_decorator.py
@@ -1,2 +1,15 @@
+import functools
+
 def retry(count):
-    pass
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(count):
+                try:
+                    return func(*args, **kwargs)
+                except ValueError:
+                    pass
+                except OSError:
+                    print(f"{func.__name__} raise OsError exception.")
+        
+        return wrapper
+    return decorator
